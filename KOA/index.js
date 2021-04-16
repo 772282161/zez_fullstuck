19dae8e0a5e047d17c53079d0aab55c194777639
772282161 <772282161@qq.com>
2021-04-16 21:42:43 +0800

    koa 补充

--- KOA/index.js
+++ KOA/index.js
@@ -1,21 +1,30 @@
 const Koa = require('./lib/application')
 const app = new Koa()
 
-app.use((ctx)=>{// 箭头函数称为中间件  ctx 对象代指本身
-    
+app.use((ctx) => { // ctx
+  console.log(ctx.req.url);
+  console.log(ctx.request.req.url);
+  console.log(ctx.response.req.url);
+
+  console.log(ctx.request.url); // ctx.req.url
+  console.log(ctx.request.path);
+  console.log(ctx.url);
+  console.log(ctx.path);
 })
 
-app.listen(3000,()=>{
-    console.log('王总嫖娼不给钱')
-}) // 不写，use内的函数参数不执行
+app.listen(3000, () => {
+  console.log('服务启动了');
+})
 
-// koa优势，让开发显著的提高开发效率，在koa 中中间件的概念特别明显，可以链式调用中间件，
-// 洋葱模型 use 方法内具备连个参数，ctx参数，next参数， listen 方法
 
 // let http = require('http')
 
-// let server= http.createServer((req,res)=>{
-//     res.end('Hello 王总嫖娼不给钱')
+// let server = http.createServer((req, res) => {
+//   res.end('Hello Jak')
 // })
 
-// cosole.log(server.listen(4000))
+// server.listen(4000)
+
+//  var test = () => {
+//   console.log(123);
+// }
