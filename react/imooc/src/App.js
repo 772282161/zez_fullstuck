589568bad524ec7629c4424d57434a07b3211e5a
772282161 <772282161@qq.com>
2021-05-04 14:14:13 +0800

    路由测试

--- react/imooc/src/App.js
+++ react/imooc/src/App.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux'
 import { addGun, removeGun, addGunAsync } from './index.redux'
 
 
+@connect(
+  //你要state 什么属性放到props里
+  state=>({num:state}),
+  // 你要什么方法，放到props里，自动dispatch
+  { addGun, removeGun, addGunAsync },
+  )
 class App extends React.Component {
-  // constructor(props) {
-  //     super(props)
-  // }
   render() {
-
     return (
       <>
         <h1>现有{this.props.num}枪</h1>
@@ -19,13 +21,5 @@ class App extends React.Component {
     )
   }
 }
-
-const mapStatetoProps = (state) => {
-  return { num: state }
-}
-
-const actionCreators = { addGun, removeGun, addGunAsync }
-
-App = connect(mapStatetoProps, actionCreators)(App)
 
 export default App
--- react/imooc/src/index.js
+++ react/imooc/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import App from './App'
+import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { counter } from './index.redux'
-
 
 const store = createStore(
   counter,
@@ -15,10 +15,40 @@ const store = createStore(
   )
 )
 
+function Erying() {
+  return <h2>二营报道</h2>
+}
+function Qibl() {
+  return <h2>骑兵营报道</h2>
+}
+class Test extends React.Component {
+  render() {
+    console.log(this.props)
+    // this.props.history.push('/')
+    return <h2>测试rul:参数{this.props.match.params.location}</h2>
+  }
+}
 
 ReactDOM.render(
   (<Provider store={store}>
-    <App />,
+    <BrowserRouter>
+      <ul>
+        <li><Link to='/'>一营</Link></li>
+        <li><Link to="/erying">二营</Link></li>
+        <li><Link to="/qibl">骑兵营</Link></li>
+      </ul>
+
+      <Switch>
+        {/* 只渲染命中的第一个路由 */}
+        {/* <Redirect to="/"></Redirect> */}
+        <Route path="/" exact component={App}></Route>
+        {/* <Route path="/:location" component={Test}></Route> */}
+        <Route path="/erying" component={Erying}></Route>
+        <Route path="/qibl" component={Qibl}></Route>
+        <Route path="/:location" component={Test}></Route>
+        {/* <App />, */}
+      </Switch>
+    </BrowserRouter>
   </Provider>),
 
   document.getElementById('root')
